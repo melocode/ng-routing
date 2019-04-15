2253042308d7274118a203ddc8fac5d85e2321ba
Lemuel <lemuel@lemuel>
2019-04-14 19:22:33 -0700

    add Authentication router guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,17 @@ import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {HomeComponent} from "./home/home.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AuthGuard} from "./auth-guard.service";
 
 const appRoutes: Routes = [
   {path: 'users', component: UsersComponent, children: [
       {path: ':id/:name', component: UserComponent}
     ]},
-  {path: 'servers', component: ServersComponent, children: [
+  {path: 'servers',
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]},
---
+++ src/app/auth.service.ts
@@ -0,0 +1,25 @@
+export class AuthService {
+  loggedIn: boolean = false;
+
+  constructor() {
+  }
+
+  isAuthenticated(): Promise<boolean> {
+    const promise = new Promise<boolean>((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+
+    return promise;
+  }
+
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
